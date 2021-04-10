df2b34a37cc637b489145ba9d1f0d591100d2a03
Sohum Khot <sohumkhot@gmail.com>
2021-04-10 22:40:17 +0530

    defined routes and user schema

--- server.js
+++ server.js
@@ -10,4 +10,11 @@ connectDB();
 
 app.get('/', (req, res) => res.send('API Running'))
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
