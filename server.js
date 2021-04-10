7d30417747c11714beb23c6c16bd8e10dbdb0d72
Sohum Khot <sohumkhot@gmail.com>
2021-04-10 20:30:48 +0530

    established mongodb connection

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'))
 
