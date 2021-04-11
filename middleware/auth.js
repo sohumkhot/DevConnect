ee2b6772445d07ecc92a0c4e180d69fc2a4a1dbb
Sohum Khot <sohumkhot@gmail.com>
2021-04-11 18:05:47 +0530

    Added login functionality and validation and fixed auth bug

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = function (req, res, next) {
   // Get token from header with key x-auth-token
-  token = req.header('x-auth-token');
+  const token = req.header('x-auth-token');
 
   // Return 401 Unauthorized if no token found
   if (!token) {
@@ -13,7 +13,8 @@ module.exports = function (req, res, next) {
   // Verify the token if token is present
   try {
     const decoded = jwt.verify(token, config.get('jwtSecret'));
-    req.user = jwt.user;
+
+    req.user = decoded.user;
     next();
   } catch (err) {
     return res.status(401).json({ msg: 'Token is not valid' });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,90 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
+// Import User from models/User
+const User = require('../../models/User');
 
 // @route   GETapi/auth
 // @desc    Test route
 // @access  Public
-router.get('/', auth, (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/users
+// @desc    Test route
+// @access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    const { email, password } = req.body;
+
+    try {
+      // Check if user exists: if not, throw 400
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Check if password matches
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      // Create payload for jwt
+      // Here id is the ObjectId associated with the MongoDB entry
+      payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      // Sign using jwt.sign for returning a token which will be used for auth in headers in the further requests
+      jwt.sign(
+        // Payload created above
+        payload,
+        // jwtSecret in default.json
+        config.get('jwtSecret'),
+        // options with expiresIn timeout to 3600s/1h
+        {
+          expiresIn: 360000,
+        },
+        // Callback for sending token as a response if no error thrown
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
