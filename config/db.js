e4d92269ccb53610e1bcd5572843a988fdf0dd75
Sohum Khot <sohumkhot@gmail.com>
2021-04-11 14:03:52 +0530

    register unique user with gravatar url and hashed password using bcrypt

--- config/db.js
+++ config/db.js
@@ -3,18 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
 
-        console.log('MongoDB Connected...');
-    }
-    catch(err) {
-        console.log(err.message);
-        process.exit(1);
-    }
-}
-
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
