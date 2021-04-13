aae6727c0ecc70f68c7f1ea722b49aa367ee8941
Sohum Khot <sohumkhot@gmail.com>
2021-04-14 00:00:41 +0530

    added PUT/api/posts/unlike/:id route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -134,4 +134,32 @@ router.put('/like/:id', auth, async (req, res) => {
   }
 });
 
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post has not been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked' });
+    }
+
+    // If post is liked, proceed
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
 module.exports = router;
